7183b533e6bd67d3f43cf3ee4bedc516adf191b9
Jessica Fu <53541136+jjessicaf@users.noreply.github.com>
2025-01-02 19:45:47 -0800

    finished coordinator

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,17 +1,20 @@
 package mr
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"net/rpc"
 	"os"
+	"sync"
+	"time"
 )
 
 type Task struct {
 	filename  string
 	status    Status
-	startTime int
+	startTime time.Time
 }
 
 type Status int
@@ -30,51 +33,91 @@ type Coordinator struct {
 	reduceTasksCompleted int
 	mapDone              bool
 	reduceDone           bool
+	mapTasksMu           sync.Mutex
+	reduceTasksMu        sync.Mutex
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
 func (c *Coordinator) RequestTask(args *RequestTaskArgs, reply *RequestTaskResponse) error {
-	// Go through files and find a filename that hasn't been assigned yet
 	if !c.mapDone {
-		for i, task := range c.mapTasks {
-			if task.status == 0 {
-				reply.Filename = task.filename
-				task.status = Assigned
-				reply.TaskType = "map"
-				reply.N = c.nReduce
-				reply.TaskNumber = i
-				return nil
-			} else if task.status == 1 {
-				// Has it taken longer than 10s?
-			} else if task.status == 2 {
-				c.mapTasksCompleted += 1
-			}
-		}
-		if c.mapTasksCompleted >= len(c.mapTasks) {
-			c.mapDone = true
+		if !c.assignTask(c.mapTasks, "map", c.nReduce, reply) {
+			return fmt.Errorf("failed to assign map task")
 		}
 	} else if !c.reduceDone {
-		for i, task := range c.reduceTasks {
-			if task.status == 0 {
-				reply.Filename = task.filename
-				task.status = Assigned
-				reply.TaskType = "reduce"
-				reply.N = len(c.mapTasks)
-				reply.TaskNumber = i
-				return nil
-			} else if task.status == 1 {
-				// Has it taken longer than 10s?
-			} else if task.status == 2 {
-				c.reduceTasksCompleted += 1
-			}
-		}
-		if c.reduceTasksCompleted >= len(c.reduceTasks) {
-			c.reduceDone = true
+		if !c.assignTask(c.reduceTasks, "reduce", len(c.mapTasks), reply) {
+			return fmt.Errorf("failed to assign reduce task")
 		}
 	}
 
 	return nil
+}
+
+func (c *Coordinator) TaskDone(args *TaskDoneArgs, reply *TaskDoneResponse) error {
+	var mu *sync.Mutex
+	if args.TaskType == "map" {
+		mu = &c.mapTasksMu
+	} else {
+		mu = &c.reduceTasksMu
+	}
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	i := args.TaskNumber
+	if args.TaskType == "map" && 0 <= i && i < len(c.mapTasks) {
+		c.mapTasks[i].status = Complete
+		c.mapTasksCompleted += 1
+		if c.mapTasksCompleted >= len(c.mapTasks) {
+			c.mapDone = true
+		}
+	} else if args.TaskType == "reduce" && 0 <= i && i < len(c.reduceTasks) {
+		c.reduceTasks[i].status = Complete
+		c.reduceTasksCompleted += 1
+		if c.reduceTasksCompleted >= len(c.reduceTasks) {
+			c.reduceDone = true
+		}
+	} else {
+		if args.TaskType != "map" && args.TaskType != "reduce" {
+			return fmt.Errorf("invalid task type: %s", args.TaskType)
+		}
+		return fmt.Errorf("invalid task number: %d", args.TaskNumber)
+	}
+
+	return nil
+}
+
+func (c *Coordinator) assignTask(tasks []Task, taskType string, n int, reply *RequestTaskResponse) bool {
+	var mu *sync.Mutex
+	if taskType == "map" {
+		mu = &c.mapTasksMu
+	} else {
+		mu = &c.reduceTasksMu
+	}
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	for i, task := range tasks {
+		if task.status == Unassigned {
+			reply.Filename = task.filename
+			tasks[i].status = Assigned // Modify the original task
+			tasks[i].startTime = time.Now()
+			reply.TaskType = taskType
+			reply.N = n
+			reply.TaskNumber = i
+			return true
+		}
+	}
+	return false
+}
+
+func (c *Coordinator) updateTasks(tasks []Task) {
+	for i := range tasks {
+		if tasks[i].status == Assigned && time.Since(tasks[i].startTime).Seconds() >= 10.0 {
+			tasks[i].status = Unassigned
+		}
+	}
 }
 
 // an example RPC handler.
@@ -103,14 +146,17 @@ func (c *Coordinator) server() {
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
 func (c *Coordinator) Done() bool {
-	ret := false
-
-	// Your code here.
-	if c.mapDone && c.reduceDone {
-		ret = true
+	if !c.mapDone {
+		c.mapTasksMu.Lock()
+		defer c.mapTasksMu.Unlock()
+		c.updateTasks(c.mapTasks)
+	} else if !c.reduceDone {
+		c.reduceTasksMu.Lock()
+		defer c.reduceTasksMu.Unlock()
+		c.updateTasks(c.reduceTasks)
 	}
 
-	return ret
+	return c.mapDone && c.reduceDone
 }
 
 // create a Coordinator.
